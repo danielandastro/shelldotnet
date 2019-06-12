b3a15d3df93d74ab5da055b336efb221a0b36f2e
Daniel Shroff <danielshroff@hotmail.com>
2019-06-12 12:55:22 +0530

    Added the plugin search method

--- SDNLib/Plugins.cs
+++ SDNLib/Plugins.cs
@@ -1,8 +1,23 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
 namespace SDNLib
 {
-    public class Class1
+    public class Plugins
     {
+        public List<PluginData> Searcher(string path)
+        {
+            var pluginList = new List<PluginData>();//declare the list
+            var temp = new PluginData();//store data before shifting to the list
+            var files = Directory.GetFiles(path, "*.sdp");//read the files from the plugin folder
+            foreach(string file in files)//process the files
+            {
+                temp.fileName = file;//filename storing
+                temp.command = file.Replace(".sdp", "");//finds command by stripping extension from filename
+                //TODO implement better metadata, and commands not limited to the filename
+                pluginList.Add(temp);//add the data to the list
+            }
+            return pluginList;//return the data
+        }
     }
 }
