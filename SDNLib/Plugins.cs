3492c17914b3e590a25440e9f939ae2792145cdf
Daniel Shroff <danielshroff@hotmail.com>
2019-06-12 13:26:23 +0530

    Added a method which compiles all plugins

--- SDNLib/Plugins.cs
+++ SDNLib/Plugins.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Diagnostics;
 using System.Collections.Generic;
 using System.IO;
 namespace SDNLib
@@ -8,7 +8,6 @@ namespace SDNLib
         public List<PluginData> Searcher(string path)
         {
             var pluginList = new List<PluginData>();//declare the list
-
             var temp = new PluginData();//store data before shifting to the list
             var files = Directory.GetFiles(path, "*.sdp");//read the files from the plugin folder
             foreach(string file in files)//process the files
@@ -19,6 +18,24 @@ namespace SDNLib
                 pluginList.Add(temp);//add the data to the list
             }
             return pluginList;//return the data
+        }
+        public void CompileAll(List<PluginData> catalog, string path)//a method to compile all known plugins in a catalog
+        {
+            #if DEBUG
+            #warning This method may be used for testing, but should not be used in production
+            #elif RELEASE
+            #warning Please rethink this desision
+            #endif
+            foreach (var file in catalog)
+            {
+                var process = Process.Start("mcs", path + "/" + file.fileName);
+                process.WaitForExit();
+            }
+            /*This is a really cheap and nasty way to improve performance on slower machines
+             * It is stupid, probably unrequired if you implement half-decent caching
+             * but it is there if required
+             * But seriously, please do not use this
+             */
         }
     }
 }
