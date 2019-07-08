5325cae56ebfe0aa0318fd5df49af99b69ca3b66
Daniel Shroff <danielshroff@hotmail.com>
2019-07-08 20:56:20 +0530

    Updating branch

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using SDNLib;
-using System.IO;
+//using SDNLib;
 namespace Executable
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var lib = new Plugins();
-            var plugins = lib.Searcher();
-            Console.WriteLine("Welcome to Shell Dot Net, version loaded is {0}", SDNLib.LibInfo.version);
-            Console.WriteLine(Directory.GetCurrentDirectory());   
+            Console.WriteLine("Hello World!");
         }
     }
 }
